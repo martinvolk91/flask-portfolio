de890f99d34f652cee7f3a6f430b814e84e0c041
Martin Volk <martinvolk91@gmail.com>
2023-11-01 15:57:32 +0100

    Model fixed (gpu to cpu), email added

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-# recommender_api.py
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
+from flask_mail import Mail, Message
+import configparser
 import implicit
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+app.secret_key = 'my_secrest_key'
 
 # Load your recommender system from the pickle file
 with open('movielens_implicit_cpu_v02.pkl', 'rb') as file:
@@ -37,6 +39,33 @@ def recommend():
 
     print(f"Output: {recommended_movies}")
     return jsonify(recommended_movies)
+
+
+@app.route('/api/contact', methods=['POST'])
+def contact():
+    config = configparser.ConfigParser()
+    config.read('config/config.ini')
+
+    mail = Mail()
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 465
+    app.config["MAIL_USE_SSL"] = True
+    app.config["MAIL_USERNAME"] = config.get('Email', 'username')
+    app.config["MAIL_PASSWORD"] = config.get('Email', 'password')
+    mail.init_app(app)
+
+    data = request.get_json()
+    print(data)
+    msg = Message(
+        subject=f"New message from your website - {data['name']}",
+        recipients=["martinvolk91@gmail.com"],
+        body=data["message"],
+        sender=data["email"],
+        reply_to=data["email"]
+    )
+
+    mail.send(msg)
+    return "Message sent!"
 
 
 if __name__ == '__main__':
